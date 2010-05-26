6411c0f39cdba343754a09fc21462a06fcaf86e0
Bob Lewis <bobl@wsu.edu>
2010-05-26 18:37:12 +0000

    "summed_octants.py" now working.  I think.

--- work/bobl/summed_octants/summed_octants.py
+++ work/bobl/summed_octants/summed_octants.py
@@ -1,9 +1,9 @@
-from numpy import ones, zeros
+from numpy import ones, zeros, array
 
-def octantWedgeSums(A):
-    N = A.shape[0]
-    assert N == A.shape[1]
-    O = zeros((8, N, N), A.dtype)
+def octantWedgeSums(a):
+    N = a.shape[0]
+    assert N == a.shape[1]
+    ows = zeros((8, N, N), a.dtype)
     # fill in the octant tables
     for j in range(N):
         jc = N - 1 - j # "j complement"
@@ -11,98 +11,113 @@ def octantWedgeSums(A):
             ic = N - 1 - i # "i complement"
 
             octant = 0
-            O[octant,i,jc] = A[i,jc]
+            ows[octant,i,jc] = a[i,jc]
             if jc < N-1:
-                O[octant,i,jc] += O[octant,i,jc+1]
+                ows[octant,i,jc] += ows[octant,i,jc+1]
                 if i > 0:
-                    O[octant,i,jc] += O[octant,i-1,jc+1]
+                    ows[octant,i,jc] += ows[octant,i-1,jc+1]
                     if jc < N-2:
-                        O[octant,i,jc] -= O[octant,i-1,jc+2]
+                        ows[octant,i,jc] -= ows[octant,i-1,jc+2]
 
             octant = 1
-            O[octant,i,jc] = A[i,jc]
+            ows[octant,i,jc] = a[i,jc]
             if i > 0:
-                O[octant,i,jc] += O[octant,i-1,jc]
+                ows[octant,i,jc] += ows[octant,i-1,jc]
                 if jc < N - 1:
-                    O[octant,i,jc] += O[octant,i-1,jc+1]
+                    ows[octant,i,jc] += ows[octant,i-1,jc+1]
                     if i > 1:
-                        O[octant,i,jc] -= O[octant,i-2,jc+1]
+                        ows[octant,i,jc] -= ows[octant,i-2,jc+1]
 
             octant = 2
-            O[octant,i,j] = A[i,j]
+            ows[octant,i,j] = a[i,j]
             if i > 0:
-                O[octant,i,j] += O[octant,i-1,j]
+                ows[octant,i,j] += ows[octant,i-1,j]
                 if j > 0:
-                    O[octant,i,j] += O[octant,i-1,j-1]
+                    ows[octant,i,j] += ows[octant,i-1,j-1]
                     if i > 1:
-                        O[octant,i,j] -= O[octant,i-2,j-1]
+                        ows[octant,i,j] -= ows[octant,i-2,j-1]
 
             octant = 3
-            O[octant,i,j] = A[i,j]
+            ows[octant,i,j] = a[i,j]
             if j > 0:
-                O[octant,i,j] += O[octant,i,j-1]
+                ows[octant,i,j] += ows[octant,i,j-1]
                 if i > 0:
-                    O[octant,i,j] += O[octant,i-1,j-1]
+                    ows[octant,i,j] += ows[octant,i-1,j-1]
                     if j > 1:
-                        O[octant,i,j] -= O[octant,i-1,j-2]
+                        ows[octant,i,j] -= ows[octant,i-1,j-2]
 
             octant = 4
-            O[octant,ic,j] = A[ic,j]
+            ows[octant,ic,j] = a[ic,j]
             if j > 0:
-                O[octant,ic,j] += O[octant,ic,j-1]
+                ows[octant,ic,j] += ows[octant,ic,j-1]
                 if ic < N - 1:
-                    O[octant,ic,j] += O[octant,ic+1,j-1]
+                    ows[octant,ic,j] += ows[octant,ic+1,j-1]
                     if j > 1:
-                        O[octant,ic,j] -= O[octant,ic+1,j-2]
+                        ows[octant,ic,j] -= ows[octant,ic+1,j-2]
 
             octant = 5
-            O[octant,ic,j] = A[ic,j]
+            ows[octant,ic,j] = a[ic,j]
             if ic < N - 1:
-                O[octant,ic,j] += O[octant,ic+1,j]
+                ows[octant,ic,j] += ows[octant,ic+1,j]
                 if j > 0:
-                    O[octant,ic,j] += O[octant,ic+1,j-1]
+                    ows[octant,ic,j] += ows[octant,ic+1,j-1]
                     if ic < N - 2:
-                        O[octant,ic,j] -= O[octant,ic+2,j-1]
+                        ows[octant,ic,j] -= ows[octant,ic+2,j-1]
 
             octant = 6
-            O[octant,ic,jc] = A[ic,jc]
+            ows[octant,ic,jc] = a[ic,jc]
             if ic < N - 1:
-                O[octant,ic,jc] += O[octant,ic+1,jc]
+                ows[octant,ic,jc] += ows[octant,ic+1,jc]
                 if jc < N - 1:
-                    O[octant,ic,jc] += O[octant,ic+1,jc+1]
+                    ows[octant,ic,jc] += ows[octant,ic+1,jc+1]
                     if ic < N - 2:
-                        O[octant,ic,jc] -= O[octant,ic+2,jc+1]
+                        ows[octant,ic,jc] -= ows[octant,ic+2,jc+1]
 
             octant = 7
-            O[octant,ic,jc] = A[ic,jc]
+            ows[octant,ic,jc] = a[ic,jc]
             if jc < N-1:
-                O[octant,ic,jc] += O[octant,ic,jc+1]
+                ows[octant,ic,jc] += ows[octant,ic,jc+1]
                 if ic < N - 1:
-                    O[octant,ic,jc] += O[octant,ic+1,jc+1]
+                    ows[octant,ic,jc] += ows[octant,ic+1,jc+1]
                     if jc < N-2:
-                        O[octant,ic,jc] -= O[octant,ic+1,jc+2]
+                        ows[octant,ic,jc] -= ows[octant,ic+1,jc+2]
 
-    return O
+    return ows
 
-N = 9
-A = ones((N,N), int)
+def applyOctangonalKernel(ows, W, d, i, j):
+    """
+    This applies a 2`W`+1 x 2`W`+1 octagonal kernel to element (`i`,
+    `j`) of an array whose octagonal wedge table is `ows`.  `d` (which
+    is between 0 and `W`) is the inset of the octagon.
+    """
+    w = W - d
+    s0 = ows[0,-1,0] + ows[1,-2,0] # sum of all elements
+    if 0:
+        inds = array(((0,i+W,j+w+1), (1,i+w-1,j+W+1),
+                (2,i-w-1,j+W), (3,i-W-1,j+w-1),
+                (4,i-W,j-w-1), (5,i-w+1,j-W-1),
+                (6,i+w+1,j-W), (7,i+W+1,j-w+1)))
+        print inds
+        # TODO: some expression like this:
+        result = s0 - sum(ows[inds].flat)
+    else:
+        result = s0 - (
+              ows[0,i+W,j+w+1] + ows[1,i+w-1,j+W+1]
+            + ows[2,i-w-1,j+W] + ows[3,i-W-1,j+w-1]
+            + ows[4,i-W,j-w-1] + ows[5,i-w+1,j-W-1]
+            + ows[6,i+w+1,j-W] + ows[7,i+W+1,j-w+1])
+    return result
 
-print A
-print
-O = octantWedgeSums(A)
-print O
-
-# sum over each octagonal kernel
-W = 3
-d = 2
-w = W - d
+N = 51
+a = ones((N,N), int)
+ows = octantWedgeSums(a)
+W = 2
+d = 0
+# TODO: a function to choose the "most circular" value of d for a
+# given W.
+# TODO: allow varying width and height
+# TODO: make "W" a full, rather than half-width
 i = N // 2
 j = N // 2
-s0 = O[0,N-1,0] + O[1,N-2,0]
-print s0
-res = s0 - (
-      O[0,i+W,j+w+1] + O[1,i,j+W+1]
-    + O[2,i-w-1,j+W] + O[3,i-W-1,j]
-    + O[4,i-W,j-w-1] + O[5,i,j-W-1]
-    + O[6,i+w+1,j-W] + O[7,i+W+1,j])
-print res
+print applyOctangonalKernel(ows, W, d, i, j)
+
